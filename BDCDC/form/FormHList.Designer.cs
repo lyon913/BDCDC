c2a5cf8458a7a070e6ba7f9216fd73b8c0c8523d
lyon913 <gzzly0913>
2017-05-05 18:08:07 +0800

    LPB

--- BDCDC/form/FormHList.Designer.cs
+++ BDCDC/form/FormHList.Designer.cs
@@ -29,31 +29,21 @@
         private void InitializeComponent()
         {
             this.group1 = new System.Windows.Forms.GroupBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.listBox2 = new System.Windows.Forms.ListBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.list_xm = new System.Windows.Forms.ListBox();
+            this.list_ljz = new System.Windows.Forms.ListBox();
+            this.dgv = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.group1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
             this.SuspendLayout();
             // 
             // group1
             // 
-            this.group1.Controls.Add(this.textBox5);
-            this.group1.Controls.Add(this.label5);
-            this.group1.Controls.Add(this.textBox4);
-            this.group1.Controls.Add(this.label4);
-            this.group1.Controls.Add(this.textBox3);
-            this.group1.Controls.Add(this.label3);
+            this.group1.Controls.Add(this.button1);
             this.group1.Controls.Add(this.textBox2);
             this.group1.Controls.Add(this.label2);
             this.group1.Controls.Add(this.textBox1);
@@ -65,37 +55,37 @@
             this.group1.TabStop = false;
             this.group1.Text = "楼盘查询";
             // 
-            // listBox1
+            // list_xm
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 12;
-            this.listBox1.Location = new System.Drawing.Point(12, 127);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(187, 424);
-            this.listBox1.TabIndex = 1;
+            this.list_xm.FormattingEnabled = true;
+            this.list_xm.ItemHeight = 12;
+            this.list_xm.Location = new System.Drawing.Point(12, 127);
+            this.list_xm.Name = "list_xm";
+            this.list_xm.Size = new System.Drawing.Size(187, 424);
+            this.list_xm.TabIndex = 1;
             // 
-            // listBox2
+            // list_ljz
             // 
-            this.listBox2.FormattingEnabled = true;
-            this.listBox2.ItemHeight = 12;
-            this.listBox2.Location = new System.Drawing.Point(205, 127);
-            this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(187, 424);
-            this.listBox2.TabIndex = 2;
+            this.list_ljz.FormattingEnabled = true;
+            this.list_ljz.ItemHeight = 12;
+            this.list_ljz.Location = new System.Drawing.Point(205, 127);
+            this.list_ljz.Name = "list_ljz";
+            this.list_ljz.Size = new System.Drawing.Size(187, 424);
+            this.list_ljz.TabIndex = 2;
             // 
-            // dataGridView1
+            // dgv
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(398, 127);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(574, 424);
-            this.dataGridView1.TabIndex = 3;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(398, 127);
+            this.dgv.Name = "dgv";
+            this.dgv.RowTemplate.Height = 23;
+            this.dgv.Size = new System.Drawing.Size(574, 424);
+            this.dgv.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 26);
+            this.label1.Location = new System.Drawing.Point(16, 27);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(65, 12);
             this.label1.TabIndex = 0;
@@ -104,6 +94,7 @@
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(87, 23);
+            this.textBox1.MaxLength = 50;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(132, 21);
             this.textBox1.TabIndex = 1;
@@ -111,75 +102,37 @@
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(321, 23);
+            this.textBox2.MaxLength = 100;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(230, 21);
+            this.textBox2.Size = new System.Drawing.Size(404, 21);
             this.textBox2.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(250, 26);
+            this.label2.Location = new System.Drawing.Point(250, 27);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(65, 12);
             this.label2.TabIndex = 2;
             this.label2.Text = "项目坐落：";
             // 
-            // textBox3
+            // button1
             // 
-            this.textBox3.Location = new System.Drawing.Point(87, 60);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(132, 21);
-            this.textBox3.TabIndex = 5;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 63);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(65, 12);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "宗地代码：";
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(321, 63);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(230, 21);
-            this.textBox4.TabIndex = 7;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(250, 66);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(65, 12);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "自然幢号：";
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(681, 23);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(230, 21);
-            this.textBox5.TabIndex = 9;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(574, 26);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(89, 12);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "不动产单元号：";
+            this.button1.Location = new System.Drawing.Point(840, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 21);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "查询项目";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // FormHList
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 563);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.listBox2);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.list_ljz);
+            this.Controls.Add(this.list_xm);
             this.Controls.Add(this.group1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -188,7 +141,7 @@
             this.Text = "楼盘表";
             this.group1.ResumeLayout(false);
             this.group1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -196,18 +149,13 @@
         #endregion
 
         private System.Windows.Forms.GroupBox group1;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.ListBox listBox2;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ListBox list_xm;
+        private System.Windows.Forms.ListBox list_ljz;
+        private System.Windows.Forms.DataGridView dgv;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
     }
 }
