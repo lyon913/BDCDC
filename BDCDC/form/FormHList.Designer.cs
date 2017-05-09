26736ad7d0ae75a65aed0526fdf3fe03836fa087
lyon913 <gzzly0913>
2017-05-09 19:19:04 +0800

    HList

--- BDCDC/form/FormHList.Designer.cs
+++ BDCDC/form/FormHList.Designer.cs
@@ -29,29 +29,29 @@
         private void InitializeComponent()
         {
             this.group1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.b_search = new System.Windows.Forms.Button();
+            this.tb_xmzl = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tb_xmmc = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.list_xm = new System.Windows.Forms.ListBox();
             this.list_ljz = new System.Windows.Forms.ListBox();
             this.dgv = new System.Windows.Forms.DataGridView();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.b_ok = new System.Windows.Forms.Button();
+            this.b_checkAll = new System.Windows.Forms.Button();
+            this.b_checkInvert = new System.Windows.Forms.Button();
+            this.b_uncheck = new System.Windows.Forms.Button();
             this.group1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
             this.SuspendLayout();
             // 
             // group1
             // 
-            this.group1.Controls.Add(this.button2);
-            this.group1.Controls.Add(this.button1);
-            this.group1.Controls.Add(this.textBox2);
+            this.group1.Controls.Add(this.b_ok);
+            this.group1.Controls.Add(this.b_search);
+            this.group1.Controls.Add(this.tb_xmzl);
             this.group1.Controls.Add(this.label2);
-            this.group1.Controls.Add(this.textBox1);
+            this.group1.Controls.Add(this.tb_xmmc);
             this.group1.Controls.Add(this.label1);
             this.group1.Location = new System.Drawing.Point(12, 12);
             this.group1.Name = "group1";
@@ -60,39 +60,40 @@
             this.group1.TabStop = false;
             this.group1.Text = "楼盘查询";
             // 
-            // button1
+            // b_search
             // 
-            this.button1.Location = new System.Drawing.Point(753, 23);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 21);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "查询项目";
-            this.button1.UseVisualStyleBackColor = true;
+            this.b_search.Location = new System.Drawing.Point(795, 23);
+            this.b_search.Name = "b_search";
+            this.b_search.Size = new System.Drawing.Size(75, 21);
+            this.b_search.TabIndex = 10;
+            this.b_search.Text = "查询";
+            this.b_search.UseVisualStyleBackColor = true;
+            this.b_search.Click += new System.EventHandler(this.b_search_Click);
             // 
-            // textBox2
+            // tb_xmzl
             // 
-            this.textBox2.Location = new System.Drawing.Point(321, 23);
-            this.textBox2.MaxLength = 100;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(404, 21);
-            this.textBox2.TabIndex = 3;
+            this.tb_xmzl.Location = new System.Drawing.Point(455, 24);
+            this.tb_xmzl.MaxLength = 100;
+            this.tb_xmzl.Name = "tb_xmzl";
+            this.tb_xmzl.Size = new System.Drawing.Size(267, 21);
+            this.tb_xmzl.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(250, 27);
+            this.label2.Location = new System.Drawing.Point(384, 27);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(65, 12);
             this.label2.TabIndex = 2;
             this.label2.Text = "项目坐落：";
             // 
-            // textBox1
+            // tb_xmmc
             // 
-            this.textBox1.Location = new System.Drawing.Point(87, 23);
-            this.textBox1.MaxLength = 50;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(132, 21);
-            this.textBox1.TabIndex = 1;
+            this.tb_xmmc.Location = new System.Drawing.Point(87, 23);
+            this.tb_xmmc.MaxLength = 50;
+            this.tb_xmmc.Name = "tb_xmmc";
+            this.tb_xmmc.Size = new System.Drawing.Size(291, 21);
+            this.tb_xmmc.TabIndex = 1;
             // 
             // label1
             // 
@@ -107,73 +108,74 @@
             // 
             this.list_xm.FormattingEnabled = true;
             this.list_xm.ItemHeight = 12;
-            this.list_xm.Location = new System.Drawing.Point(12, 91);
+            this.list_xm.Location = new System.Drawing.Point(12, 115);
             this.list_xm.Name = "list_xm";
-            this.list_xm.Size = new System.Drawing.Size(187, 460);
+            this.list_xm.Size = new System.Drawing.Size(187, 436);
             this.list_xm.TabIndex = 1;
+            this.list_xm.SelectedValueChanged += new System.EventHandler(this.list_xm_SelectedValueChanged);
             // 
             // list_ljz
             // 
             this.list_ljz.FormattingEnabled = true;
             this.list_ljz.ItemHeight = 12;
-            this.list_ljz.Location = new System.Drawing.Point(205, 91);
+            this.list_ljz.Location = new System.Drawing.Point(205, 115);
             this.list_ljz.Name = "list_ljz";
-            this.list_ljz.Size = new System.Drawing.Size(187, 460);
+            this.list_ljz.Size = new System.Drawing.Size(187, 436);
             this.list_ljz.TabIndex = 2;
             // 
             // dgv
             // 
             this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv.Location = new System.Drawing.Point(398, 118);
+            this.dgv.Location = new System.Drawing.Point(398, 115);
             this.dgv.Name = "dgv";
             this.dgv.RowTemplate.Height = 23;
-            this.dgv.Size = new System.Drawing.Size(574, 433);
+            this.dgv.Size = new System.Drawing.Size(574, 436);
             this.dgv.TabIndex = 3;
             // 
-            // button2
+            // b_ok
             // 
-            this.button2.Location = new System.Drawing.Point(847, 23);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 21);
-            this.button2.TabIndex = 11;
-            this.button2.Text = "确定";
-            this.button2.UseVisualStyleBackColor = true;
+            this.b_ok.Location = new System.Drawing.Point(879, 23);
+            this.b_ok.Name = "b_ok";
+            this.b_ok.Size = new System.Drawing.Size(75, 21);
+            this.b_ok.TabIndex = 11;
+            this.b_ok.Text = "确定";
+            this.b_ok.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // b_checkAll
             // 
-            this.button3.Location = new System.Drawing.Point(807, 91);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(51, 21);
-            this.button3.TabIndex = 12;
-            this.button3.Text = "全选";
-            this.button3.UseVisualStyleBackColor = true;
+            this.b_checkAll.Location = new System.Drawing.Point(807, 91);
+            this.b_checkAll.Name = "b_checkAll";
+            this.b_checkAll.Size = new System.Drawing.Size(51, 21);
+            this.b_checkAll.TabIndex = 12;
+            this.b_checkAll.Text = "全选";
+            this.b_checkAll.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // b_checkInvert
             // 
-            this.button4.Location = new System.Drawing.Point(864, 91);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(51, 21);
-            this.button4.TabIndex = 13;
-            this.button4.Text = "反选";
-            this.button4.UseVisualStyleBackColor = true;
+            this.b_checkInvert.Location = new System.Drawing.Point(864, 91);
+            this.b_checkInvert.Name = "b_checkInvert";
+            this.b_checkInvert.Size = new System.Drawing.Size(51, 21);
+            this.b_checkInvert.TabIndex = 13;
+            this.b_checkInvert.Text = "反选";
+            this.b_checkInvert.UseVisualStyleBackColor = true;
             // 
-            // button5
+            // b_uncheck
             // 
-            this.button5.Location = new System.Drawing.Point(921, 91);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(51, 21);
-            this.button5.TabIndex = 14;
-            this.button5.Text = "不选";
-            this.button5.UseVisualStyleBackColor = true;
+            this.b_uncheck.Location = new System.Drawing.Point(921, 91);
+            this.b_uncheck.Name = "b_uncheck";
+            this.b_uncheck.Size = new System.Drawing.Size(51, 21);
+            this.b_uncheck.TabIndex = 14;
+            this.b_uncheck.Text = "不选";
+            this.b_uncheck.UseVisualStyleBackColor = true;
             // 
             // FormHList
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 563);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.b_uncheck);
+            this.Controls.Add(this.b_checkInvert);
+            this.Controls.Add(this.b_checkAll);
             this.Controls.Add(this.dgv);
             this.Controls.Add(this.list_ljz);
             this.Controls.Add(this.list_xm);
@@ -197,13 +199,13 @@
         private System.Windows.Forms.ListBox list_ljz;
         private System.Windows.Forms.DataGridView dgv;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox tb_xmzl;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox tb_xmmc;
+        private System.Windows.Forms.Button b_search;
+        private System.Windows.Forms.Button b_ok;
+        private System.Windows.Forms.Button b_checkAll;
+        private System.Windows.Forms.Button b_checkInvert;
+        private System.Windows.Forms.Button b_uncheck;
     }
 }
--- BDCDC/form/FormHList.cs
+++ BDCDC/form/FormHList.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BDCDC.model;
+using BDCDC.service;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,49 @@ namespace BDCDC.form
 {
     public partial class FormHList : Form
     {
+        private XmService xs = new XmService();
+        private List<H> selected = null;
+        
+
         public FormHList()
         {
             InitializeComponent();
+            init();
+        }
+
+        public void init()
+        {
+            initUI();
+        }
+
+        private void initUI()
+        {
+            list_xm.DisplayMember = "XMMC";
+        }
+
+        private void b_search_Click(object sender, EventArgs e)
+        {
+            string key_xmmc = tb_xmmc.Text;
+            string key_xmlz = tb_xmzl.Text;
+
+            if(String.IsNullOrEmpty(key_xmmc) && String.IsNullOrEmpty(key_xmlz))
+            {
+                MessageBox.Show(this, "请填写查询条件","错误");
+                return;
+            }
+
+            List<XM> xmList = xs.search(key_xmmc, null, key_xmlz);
+
+        }
+
+        private void loadXmList(List<XM> list)
+        {
+            list_xm.DataSource = list;
+        }
+
+        private void list_xm_SelectedValueChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
